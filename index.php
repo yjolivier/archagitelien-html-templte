05559f56a66275b4c5f1bf52423e85932a132227
yjolivier <bouxouyao@gmail.com>
2019-12-19 00:02:21 +0000

    Complete the customization of the slide and start of the article section

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 		<!--Font-->
 		<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
@@ -56,19 +57,31 @@
 						<div class="carousel-item active">
 							<img class="d-block w-100" src="img/img4.jpg" alt="First slide">
 							  <div class="carousel-caption d-none d-md-block">
-									<h5>PAR CE QUE VOUS EN AVEZ BESOIN, NOUS VOUS AIDONS</h5>
+									<h1>PAR CE QUE VOUS EN AVEZ BESOIN, NOUS VOUS AIDONS</h1>
+									<div class="carousel-btn-container">
+										<a class="btn-carousel" href="#">A propos</a>
+										<a class="btn-carousel btn-2" href="#">Voir l'article</a>
+        					</div>
 								</div>
 						</div>
 						<div class="carousel-item">
 							<img class="d-block w-100" src="img/img5.jpg" alt="Second slide">
 							<div class="carousel-caption d-none d-md-block">
-								<h5>PAR CE QUE VOUS EN AVEZ BESOIN, NOUS VOUS AIDONS</h5>
+								<h1>L'emprunt n'est pas un don. Le don vient du coeur, non de la fortune. </h1>
+								<div class="carousel-btn-container">
+									<a class="btn-carousel" href="#">A propos</a>
+									<a class="btn-carousel btn-2" href="#">Voir l'article</a>
+        				</div>
 							</div>
 						</div>
 						<div class="carousel-item">
 							<img class="d-block w-100" src="img/img6.jpg" alt="Third slide">
 							<div class="carousel-caption d-none d-md-block">
-								<h5>PAR CE QUE VOUS EN AVEZ BESOIN, NOUS VOUS AIDONS</h5>
+								<h1>Cache tes dons, ainsi le Nil cache sa source.</h1>
+								<div class="carousel-btn-container">
+									<a class="btn-carousel" href="#">A propos</a>
+									<a class="btn-carousel btn-2" href="#">Voir l'article</a>
+        				</div>
 							</div>
 						</div>
 					</div>
@@ -81,6 +94,76 @@
 						<span class="sr-only">Next</span>
 					</a>
 				</div>
+			</section>
+			<section class="section-evenement row">
+				<div class="section-evenement-title col-md-12 col-lg-12 col-sm-12">
+					<h3>Le Lorem Ipsum est simplement du faux</h3>
+					<p> texte employé dans la composition texte employé dans la composition texte employé dans la composition</p>
+				</div>
+				<div class="row block-article col-md-12 col-lg-12 col-sm-12">
+					<div class="card article col-lg-4 col-md-6">
+						<div class="card-container">  
+							<div class="card-img">
+								<img src="http://localhost/elogebenao/wp-content/uploads/2019/09/img2-660x650.jpg" class="attachment-post-thumb size-post-thumb wp-post-image" alt="" width="660" height="650">      
+							</div>
+							<div class="card-body">
+								<div class="card-text">
+									<h5>La  valeur d’un homme</h5>
+									<p>
+										La  valeur d’un homme ne se mesure pas à son argent, son statut ou ses  possessions. La valeur d’un homme réside...
+									</p>
+								</div>
+								<center>
+									<div class="btn-article">
+										<a href="http://localhost/elogebenao/2019/09/14/la-valeur-dun-homme/">Voir plus</a>
+									</div>
+								</center>
+							</div>
+						</div>
+					</div>
+					<div class="card article col-lg-4 col-md-6">
+						<div class="card-container">  
+							<div class="card-img">
+								<img src="http://localhost/elogebenao/wp-content/uploads/2019/09/img5-660x650.jpg" class="attachment-post-thumb size-post-thumb wp-post-image" alt="" width="660" height="650">      
+							</div>
+							<div class="card-body">
+								<div class="card-text">
+									<h5>Les jeunes  aiment le luxe</h5>
+									<p>
+										Nos jeunes
+										aiment le luxe, ont de mauvaises manières, se moquent de l’autorité et 
+										n’ont aucun respect pour...
+									</p>
+								</div>
+								<center>
+									<div class="btn-article">
+										<a href="http://localhost/elogebenao/2019/09/13/les-jeunes-aiment-le-luxe/">Voir plus</a>
+									</div>
+								</center>
+							</div>
+						</div>
+					</div>
+					<div class="card article col-lg-4 col-md-6">
+						<div class="card-container">  
+							<div class="card-img">
+								<img src="http://localhost/elogebenao/wp-content/uploads/2019/09/img7-660x565.jpg" class="attachment-post-thumb size-post-thumb wp-post-image" alt="" width="660" height="565">      </div>
+								<div class="card-body">
+									<div class="card-text">
+										<h5>La reussite et la patience</h5>
+										<p>
+											La réussite est liée à la patience mais elle dépend également de beaucoup de bonne volonté. smiling african...
+										</p>
+									</div>
+									<center>
+										<div class="btn-article">
+											<a href="http://localhost/elogebenao/2019/09/13/la-reussite-et-la-patience/">Voir plus</a>
+										</div>
+									</center>
+								</div>
+							</div>
+						</div>		
+					</div>
+				</div>	
 			</section>
 		</div>
 		<!-- Script -->
