f9583f3d1a7b3b1508549d63a95f463b5fe44a34
yjolivier <bouxouyao@gmail.com>
2019-12-24 03:08:28 +0000

    Establishment of the partner and gallery section

--- index.php
+++ index.php
@@ -176,9 +176,58 @@
 					</div>
 				</div>
 			</section>
-			<section class="sponsor-section">
+			<section class="form-section">
 				
 			</section>
+			<section class="partner-section row">
+				<div class="partner-section-title col-md-12 col-lg-12 col-sm-12">
+					<h3>partenaire</h3>
+				</div>
+				<div class="col partner-img">
+					<img src="img/img13.png" alt="..." class="rounded-circle">
+				</div>
+				<div class="col partner-img">
+					<img src="img/img11.png" alt="..." class="rounded-circle">
+				</div>
+				<div class="col partner-img">
+					<img src="img/img12.png" alt="..." class="rounded-circle">
+				</div>
+				<div class="col partner-img">
+					<img src="img/img10.jpg" alt="..." class="rounded-circle">
+				</div>
+				<div class="col partner-img">
+					<img src="img/img19.png" alt="..." class="rounded-circle">
+				</div>
+			</section>
+			<section class="galerie row">
+				<div class="galerie-section-title col-md-12 col-lg-12 col-sm-12">
+					<h3>galerie</h3>
+				</div>
+				<div class="col galerie-img">
+					<img src="img/img14.jpg" alt="..." class="rounded-0">
+				</div>
+				<div class="col galerie-img">
+					<img src="img/img15.jpg" alt="..." class="rounded-0">
+				</div>
+				<div class="col galerie-img">
+					<img src="img/img16.jpg" alt="..." class="rounded-0">
+				</div>
+				<div class="col galerie-img">
+					<img src="img/img17.jpg" alt="..." class="rounded-0">
+				</div>
+				<div class="col galerie-img">
+					<img src="img/img14.jpg" alt="..." class="rounded-0">
+				</div>
+				<div class="col galerie-img">
+					<img src="img/img14.jpg" alt="..." class="rounded-0">
+				</div>
+				<div class="col galerie-img">
+					<img src="img/img16.jpg" alt="..." class="rounded-0">
+				</div>
+			</section>
+			<footer class="row">
+
+			</footer>
 		</div>
 		<!-- Script -->
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
